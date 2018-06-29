e8a7a1cd68151cc25f2801c8bd908b72688ad7d8
SudhindraNayak <nayaksudhindra1996@gmail.com>
2018-06-29 15:51:07 +0530

    29th June 3:50

--- server.js
+++ server.js
@@ -5,32 +5,38 @@ MongoClient = require('mongodb').MongoClient,
 assert = require('assert'),
 bodyparser=require('body-parser'),
 path=require('path'),
-url="mongodb://13.229.175.200:27017/delvia";
+url="mongodb://delvia:delvia13.229.175.200:27017/delvia";
 
-MongoClient.connect(url,function(err,db){
-	if(err)throw err;
-	let dbo=db.db("Delvia");
+MongoClient.connect('mongodb://delvia:delvia@13.229.175.200:27017/delvia', function(err, conn) {
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+    let db=conn.db('delvia');
 	app.get('/', function(req,res){
-		var query={username:req.query.admin};
-		dbo.collection("events").find(query).toArray(function(err,result){
-			if(err) throw err;
-			if(result.length>0)
-			{
-				res.json(result.recordset);
-			}
-			else
-			{
-				res.json({Error:"No Data Found"});
-			}
-			db.close();
-		});
+		//var query={username:req.query.admin};
+		
+		 db.collection('events').find().limit(300).toArray(function(err, docs) {
+            console.log(docs);
+            res.json(docs) ;
+        });
+		// dbo.collection("events").find(query).toArray(function(err,result){
+			// if(err) throw err;
+			// if(result.length>0)
+			// {
+				// res.json(result.recordset);
+			// }
+			// else
+			// {
+				// res.json({Error:"No Data Found"});
+			// }
+			// db.close();
+		// });
 	});
 	
 	app.use(bodyparser.json());
 
 	app.post('/',function(req,res){
 		console.log(req.body);
-		dbo.collection("events").insertOne(req.body,function(err,result){
+		db.collection("events").insertOne(req.body,function(err,result){
 			if(err)
 			{
 				res.json({status:"fail"});
@@ -45,7 +51,7 @@ MongoClient.connect(url,function(err,db){
         res.sendStatus(404);
     });
  
-    var server = app.listen(3000, function() { 
+    var server = app.listen(3040, function() { 
         var port = server.address().port;
         console.log('Express server listening on port %s.', port);
     });
